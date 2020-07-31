b751198e6e17435e02b36adb35608233af281d83
wahbadb94 <dustin.wahba@gmail.com>
2020-07-31 16:01:05 -0400

    Fix Bottom Boarder. Add horizontal cell scaling.

--- MinesweeperCL/Views/BoardView.cs
+++ MinesweeperCL/Views/BoardView.cs
@@ -8,6 +8,10 @@ namespace MinesweeperCL
     {
         private const int CellWidth = 4;    // a location on the board is 4 characters wide on the console
         private const int CellHeight = 2;   // a location on the board is 2 lines tall on the console
+        
+        // padding to left and right of cell display text based on CellWidth
+        private const int PaddingL = CellWidth / 2 - 1;
+        private const int  PaddingR = (CellWidth / 2 + CellWidth % 2) - 1;
 
         public static void View(Board board, Point playerPosition)
         {
@@ -20,16 +24,28 @@ namespace MinesweeperCL
                 }
             }
 
+            // draw bottom boarder
+            Console.SetCursorPosition(0, board.Size * CellHeight);
+            Console.Write(new String('\'', board.Size * CellWidth + 1));
+
             // place cursor at player's position
             PlaceCursor(playerPosition);
         }
 
         public static void DisplayLocation(BoardLocation location)
         {
-            Console.SetCursorPosition(location.X*CellWidth, location.Y*CellHeight);
-            Console.Write($"|{new String('\'', CellWidth-1)}|");
+            string displayCharacter = "o";
+
+            // translate to location
+            Console.SetCursorPosition(location.X * CellWidth, location.Y * CellHeight);
+
+            // draw upper boarder
+            Console.Write($"|{new String('\'', CellWidth - 1)}|");
+
+            // draw contents
             Console.SetCursorPosition(location.X*CellWidth, location.Y*CellHeight+1);
-            Console.Write($"|{location.X}" + new string(' ', CellWidth - 3) + $"{location.Y}|");
+            Console.Write($"|{new String(' ', PaddingL)}{displayCharacter}{new String(' ', PaddingR)}|");
+
         }
 
         private static void PlaceCursor(Point currentLocation)
@@ -38,7 +54,7 @@ namespace MinesweeperCL
             Console.SetCursorPosition(
                 currentLocation.X * CellWidth + CellWidth / 2,
                 currentLocation.Y * CellHeight + CellHeight / 2
-                );
+            );
         }
     }
 }
